44685a1d022555f3d3610d4e60ef8f5fa4f080a6
ThatsNotMe <mineflax@gmail.com>
2024-10-15 15:23:23 +0200

    übung 2 done Implemented BrokerTask for Parallel Request Handling and Poison-Pill to stop the Broker Module

--- aqua/blatt1/broker/Broker.java
+++ aqua/blatt1/broker/Broker.java
@@ -1,5 +1,9 @@
 package aqua.blatt1.broker;
 
+import aqua.blatt1.common.Direction;
+import aqua.blatt1.common.FishModel;
+import aqua.blatt2.broker.PoisonPill;
+import aqua.blatt2.broker.Poisoner;
 import messaging.Endpoint;
 import messaging.Message;
 
@@ -10,68 +14,98 @@ import aqua.blatt1.common.msgtypes.DeregisterRequest;
 import aqua.blatt1.common.msgtypes.RegisterRequest;
 
 import java.net.InetSocketAddress;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Broker {
-    Endpoint endpoint;
-
-    ClientCollection<InetSocketAddress> client_list;
-
-    int client_count = 0;
+    private Endpoint endpoint;
+    private ClientCollection<InetSocketAddress> client_list;
+    private int client_count = 0;
+    private static ExecutorService thread_pool = Executors.newFixedThreadPool(8);
+    private static volatile boolean stopRequested = false;
 
     public Broker() {
         this.endpoint = new Endpoint(4711);
         this.client_list = new ClientCollection<>();
     }
 
-    private void register(Message msg) {
-        InetSocketAddress curr_client = null;
-        try {
-            curr_client = client_list.getClient(client_list.indexOf(msg.getSender()));
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Client list is Empty");
+    private class BrokerTask implements Runnable {
+        private Message msg;
+
+        public BrokerTask(Message msg) {
+            this.msg = msg;
         }
 
-        if (curr_client != null) {
-            System.out.println("Client " + curr_client + " already registered");
-        } else {
-            client_list.add("tank" + client_count, msg.getSender());
-            System.out.println("Client " + msg.getSender() + " got registered as tank" + client_count);
-            endpoint.send(msg.getSender(), new RegisterResponse("tank" + client_count));
-            client_count++;
+        @Override
+        public void run() {
+            if (msg.getPayload() instanceof RegisterRequest) {
+                register(msg);
+            }
+            if (msg.getPayload() instanceof DeregisterRequest) {
+                deregister(msg);
+            }
+            if (msg.getPayload() instanceof HandoffRequest) {
+                handoffFish(msg);
+            }
+        }
+
+        private void register(Message msg) {
+            InetSocketAddress curr_client = null;
+            try {
+                curr_client = client_list.getClient(client_list.indexOf(msg.getSender()));
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Client list is Empty");
+            }
+
+            if (curr_client != null) {
+                System.out.println("Client " + curr_client + " already registered");
+            } else {
+                client_list.add("tank" + client_count, msg.getSender());
+                System.out.println("Client " + msg.getSender() + " got registered as tank" + client_count);
+                endpoint.send(msg.getSender(), new RegisterResponse("tank" + client_count));
+
+                client_count++;
+            }
+        }
+
+        private void deregister(Message msg) {
+            int curr_client_index = client_list.indexOf(msg.getSender());
+            client_list.remove(curr_client_index);
+            System.out.println("Client tank" + curr_client_index + " got succesfully removed");
+        }
+
+        private void handoffFish(Message msg) {
+            int curr_client_index = client_list.indexOf(msg.getSender());
+            FishModel fish = ((HandoffRequest) msg.getPayload()).getFish();
+            if (fish.getDirection() == Direction.LEFT) {
+                endpoint.send(client_list.getLeftNeighorOf(curr_client_index), new HandoffRequest(((HandoffRequest) msg.getPayload()).getFish()));
+            } else {
+                endpoint.send(client_list.getRightNeighorOf(curr_client_index), new HandoffRequest(((HandoffRequest) msg.getPayload()).getFish()));
+            }
         }
     }
 
-    private void deregister(Message msg) {
-        int curr_client_index = client_list.indexOf(msg.getSender());
-        client_list.remove(curr_client_index);
-        System.out.println("Client tank" + curr_client_index + " got succesfully removed");
-    }
+    private void broker() {
 
-    private void handoffFish(Message msg) {
-        int curr_client_index = client_list.indexOf(msg.getSender());
-        int next_client_index = (curr_client_index + 1) % client_list.size();
+        new Thread(() -> Poisoner.main(null)).start();
 
-        System.out.println("Next hand off Client is tank" + next_client_index);
-        endpoint.send(client_list.getClient(next_client_index), new HandoffRequest(((HandoffRequest) msg.getPayload()).getFish()));
-
-    }
-
-    public void broker() {
-        Message message;
+        Message msg;
         while(true) {
+            msg = endpoint.blockingReceive();
 
-            message = endpoint.blockingReceive();
+            if (msg.getPayload() instanceof PoisonPill) {
+                for (int i = 0; i < client_list.size(); i++){
+                    endpoint.send(client_list.getClient(i), new PoisonPill());
+                }
 
-            if (message.getPayload() instanceof RegisterRequest) {
-                register(message);
+                stopRequested = true;
+                break;
             }
-            if (message.getPayload() instanceof DeregisterRequest) {
-                deregister(message);
-            }
-            if (message.getPayload() instanceof HandoffRequest) {
-                handoffFish(message);
-            }
+            thread_pool.execute(new BrokerTask(msg));
         }
+        thread_pool.shutdown();
     }
 
     public static void main(String[] args) {
